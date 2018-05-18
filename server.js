ed22c990ba319b3af4dd3850a289dda3388ecc3b
Pawan Kumar Singh <pspsingh972@gmail.com>
2018-05-19 04:46:36 +0530

    Server Side Code Fix for Deployment to Heroku

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve only the static files form the dist directory app.use(express.static(__dirname + '/dist'));
-
+// Serve only the static files form the dist directory 
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
 
 res.sendFile(path.join(__dirname+'/dist/index.html')); });
